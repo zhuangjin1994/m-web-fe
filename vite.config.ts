c1b13755a75936b67a7c8706c02b0093ac443b40
庄进 <zhuangjin@szkingdom.com>
2022-04-20 15:13:50 +0800

    重新上传dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:'/mangosteen/dist/',
+  base: '/mangosteen-fe/dist/',
   plugins: [
     vue(),
     vueJsx({
