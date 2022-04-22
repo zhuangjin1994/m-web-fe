584e8c73c2530686676f42e21fe0653ad111ad76
庄进 <zhuangjin@szkingdom.com>
2022-04-22 18:26:38 +0800

    添加欢迎页以及子页面

---
+++ src/views/welcome/Welcome.tsx
@@ -0,0 +1,16 @@
+import { defineComponent } from "vue";
+import { RouterView } from "vue-router";
+import s from './Welcome.module.scss'
+import logo from '../../assets/icons/mangosteen.svg'
+export const Welcome = defineComponent({
+    setup(props,context) {
+        return () => <div class={s.welcome}>
+            <header>
+                <img src={logo} />
+                <h1>山竹</h1>
+            </header>
+            <main class={s.main}><RouterView /></main>
+            {/* <footer>button</footer> */}
+        </div>
+    }
+})
