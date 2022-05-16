1e52fc9f8ababaef2ea3dee96dc2da7e86c3450f
庄进 <zhuangjin@szkingdom.com>
2022-05-17 02:48:54 +0800

    添加Navbar组件

--- src/views/SrartPage/StartPage.tsx
+++ src/views/SrartPage/StartPage.tsx
@@ -3,6 +3,7 @@ import { Button } from "../../shared/Button/Button";
 import { Center } from "../../shared/Center/Center";
 import { FloatButton } from "../../shared/FloatButton/FloatButton";
 import { Icon } from "../../shared/Icon/Icon";
+import { NavBar } from "../../shared/NavBar/NavBar";
 import  s  from "./StartPage.module.scss";
 export const StartPage = defineComponent({
     setup(props, context) {
@@ -11,12 +12,18 @@ export const StartPage = defineComponent({
         }
         return () => (
             <div>
-                <nav class={s.menu}>menu</nav>
+                {/* <nav class={s.menu}>menu</nav> */}
+                <NavBar>
+                    {{
+                        default: () => '山竹记账',
+                        icon: (_class: string) => <Icon class={_class} name="menu" />
+                    }}
+                </NavBar>
                 <Center class={s.pig_wrapper}>
                     <Icon name="pig" class={s.pig} />
                 </Center>
                 <div class={s.button_wrapper}>
-                    <Button class={s.button} onClick={onClick} >按钮</Button>
+                    <Button class={s.button} onClick={onClick} >开始记账</Button>
                 </div>
                 <FloatButton iconName="add" />
             </div>
