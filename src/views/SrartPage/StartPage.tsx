233065c169c1927c7309febe63defebdd8aab539
庄进 <zhuangjin@szkingdom.com>
2022-05-17 01:43:30 +0800

    创建FloatButton 组件

--- src/views/SrartPage/StartPage.tsx
+++ src/views/SrartPage/StartPage.tsx
@@ -1,5 +1,6 @@
 import { defineComponent } from "vue";
 import { Button } from "../../shared/Button/Button";
+import { FloatButton } from "../../shared/FloatButton/FloatButton";
 import  s  from "./StartPage.module.scss";
 export const StartPage = defineComponent({
     setup(props, context) {
@@ -7,8 +8,11 @@ export const StartPage = defineComponent({
             console.log('111')
         }
         return () => (
-            <div class={s.button_wrapper}>
-                <Button class={s.button} onClick={onClick} >按钮</Button>
+            <div>
+                <div class={s.button_wrapper}>
+                    <Button class={s.button} onClick={onClick} >按钮</Button>
+                </div>
+                <FloatButton iconName="add" />
             </div>
         )
     }
