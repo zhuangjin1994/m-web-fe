c1278adc9ede629b929a095801ffa8592799e564
庄进 <zhuangjin@szkingdom.com>
2022-06-01 01:57:46 +0800

    完成表单验证

---
+++ src/shared/validate.tsx
@@ -0,0 +1,42 @@
+interface FData {
+    [k:string]:number|string|undefined|null|FData
+}
+
+type Rule<T> = {
+    key: keyof T,
+    message: string,
+} & (
+    { type: 'required' } |
+    { type: 'pattern', regex:RegExp}
+)
+type Rules<T> = Rule<T>[];
+export type { Rules, Rule, FData }
+export const validate = <T extends FData>(formData: T, rules: Rules<T>) => {
+    type Errors = {
+        [k in keyof T]?: string[]
+    }
+    const errors: Errors = {}
+    console.log(rules)
+    rules.map(rule => {
+        const {key, type, message} = rule
+        const value = formData[key]
+        switch (type) {
+            case 'required':
+                if (value === undefined || value === null || value === '') {
+                    errors[key] = errors[key] ?? []
+                    errors[key]?.push(message)
+                }
+                break;
+            case 'pattern':
+                if (value && !rule.regex.test(value.toString())) {
+                    errors[key] = errors[key] ?? []
+                    errors[key]?.push(message)
+                }
+                break;
+            default:
+            return
+        }
+        console.log(errors)
+    })
+    return errors;
+}
